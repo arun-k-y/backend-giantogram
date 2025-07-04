2a291817acf1e7aac6fbf39117243f3b91587ae1
arun <you@example.com>
2025-07-04 22:07:50 +0530

    commit

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -6,9 +6,9 @@ if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !pr
 }
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "daaavzzzf",
-  api_key: process.env.CLOUDINARY_API_KEY || "189183574785781",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "T8Yw9tvkLnCoxfSuhbTyPGcwZBo",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
+  api_key: process.env.CLOUDINARY_API_KEY ,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
